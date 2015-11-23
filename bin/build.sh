a1ac8db9a102abbe0b89d0474bfbc1d5f6d756a2
Christian Betz <christian.betz@performance-media.de>
2015-11-23 12:50:09 +0100

    adds hostname, build-/run-scripts

---
+++ bin/build.sh
@@ -0,0 +1,3 @@
+capstan delete riemann.osv
+capstan build -v -p vbox
+VBoxManage internalcommands sethduuid ~/.capstan/repository/riemann.osv/riemann.osv.vbox f01ba77d-3be1-40da-bb26-9a40ebd19327
---
+++ bin/run.sh
@@ -0,0 +1,1 @@
+capstan run riemann.osv -v -p vbox -f 5555:5555 -f 5556:5556 -f 8001:8000 

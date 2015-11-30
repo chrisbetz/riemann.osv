658e3afc83ca82d24359e87ebe27dab8ab581321
Christian Betz <christian.betz@performance-media.de>
2015-11-30 09:57:24 +0100

    add ability to remove artifacts before building new ones.

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,4 @@
 capstan delete riemann.osv
+rm -rf ~/.capstan/repository/riemann.osv/
 capstan build -v -p vbox
 VBoxManage internalcommands sethduuid ~/.capstan/repository/riemann.osv/riemann.osv.vbox f01ba77d-3be1-40da-bb26-9a40ebd19327

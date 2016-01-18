37f442c0abf313263b17e434183aff10b4e9c78c
Christian Betz <christian.betz@performance-media.de>
2016-01-18 11:42:13 +0100

    fixes internal network configuration

--- bin/addinternalnetwork.sh
+++ bin/addinternalnetwork.sh
@@ -1,1 +1,5 @@
+VBoxManage controlvm riemann.osv poweroff
 VBoxManage modifyvm "riemann.osv" --intnet2 "virtualcloud" --nic2 intnet --nictype2 virtio --cableconnected2 on --nicpromisc2 allow-all
+VBoxManage startvm riemann.osv --type headless
+
+curl 'http://localhost:8001/network/ifconfig/eth1' -s | jq '.config.addr' | xargs echo -n; echo '  riemann'
--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,1 @@
-capstan run riemann.osv -v -p vbox -f 5555:5555 -f 5556:5556 -f 8001:8000 
+capstan run riemann.osv -v -p vbox -f 5555:5555 -f 5556:5556 -f 5557:5557 -f 5558:5558  -f 8001:8000
